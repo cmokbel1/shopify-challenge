dadcb027447e360b25eefff7c8cc82f06e076319
Claude Mokbel <claudemokbel@gmail.com>
2022-05-18 15:16:29 -0700

    added bootstrap theme and gitignore

--- src/App.js
+++ src/App.js
@@ -2,33 +2,67 @@ import { useState } from 'react';
 
 
 function App() {
-  const [response,setResponse] = useState()
-  async function onSubmit(event) {
+  const [userPrompt, setUserPrompt] = useState('');
+  const [response, setResponse] = useState({
+    userPrompt: '',
+    aiResponse: '',
+  });
 
+  const apiKey = process.env.SECRET_KEY;
+  const aiURL = 'https://api.openai.com/v1/engines/text-curie-001/completions';
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const data = {
+      prompt: userPrompt,
+      temperature: 0.6,
+      max_tokens: 64,
+      echo: true
+    }
+    try {
+      const response = await fetch(aiURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        // change our response from the API call to an object
+        const jsonResponse = await response.json();
+        console.log(jsonResponse)
+        // We want to prepend our findings from the API to the responses section
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  return (
-    <>
-      <h1 className="text-light text-center bg-info">AI Sandbox</h1>
-      <br />
-      <div className="container">
-        <h3 className="text-left">Instructions:</h3>
-        <p>Insert a prompt to the AI and see what the computer has to tell to you</p>
-        <form method="POST" onSubmit={onSubmit} className="mb-3">
-          <label for="PromptAI" className="form-label badge rounded-pill text-bg-primary">Enter Prompt</label>
-          <textarea className="form-control" id="PromptAi" rows="3"></textarea>
-          <br />
-          <input className="btn btn-primary" type="submit" id="query">Submit</input>
-        </form>
+return (
+  <>
+    <h1 className="text-light text-center bg-info shadow-lg p-3 mb-5">AI Sandbox</h1>
+    <br />
+    <div className="container">
+      <h3 className="text-left shadow p-3 mb-5 bg-body rounded text-uppercase">Instructions</h3>
+      <h5>Insert a prompt to the AI and see what the computer has to tell to you</h5>
+      <form method="POST" onSubmit={onSubmit} className="mb-3">
+        <label htmlFor="PromptAI" className="form-label badge rounded-pill text-bg-primary">Enter Prompt</label>
+        <textarea className="form-control" id="PromptAi" rows="3" onChange={(e) => setUserPrompt(e.target.value)} placeholder="Name a famous actor from the early 1900's" value={userPrompt}></textarea>
+        <br />
+        <input className="btn btn-primary" type="submit" id="query"></input>
+      </form>
+    </div>
+    <br />
+    <div className="container">
+      <h4 className="text-uppercase shadow p-3 mb-5 bg-body rounded">Responses</h4>
+      <div className="container" id="responses">
       </div>
-      <br />
-      <div class="container">
-        <h4 class="text-uppercase">Responses</h4>
-        <div class="container" id="responses">
-        </div>
-      </div>
-    </>
-  );
+    </div>
+  </>
+);
 }
 
 export default App;
