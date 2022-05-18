17282b2f68e8741eb91d1f54454d10437b048d6c
Claude Mokbel <claudemokbel@gmail.com>
2022-05-18 15:35:33 -0700

    added placeholder for textarea

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { useState } from 'react';
 
 function App() {
   const [userPrompt, setUserPrompt] = useState('');
-  const [response, setResponse] = useState({
+  const [responses, setResponses] = useState({
     userPrompt: '',
     aiResponse: '',
   });
 
   const apiKey = process.env.SECRET_KEY;
-  const aiURL = 'https://api.openai.com/v1/engines/text-curie-001/completions';
+  const aiURL = 'https://api.openai.com/v1/completions';
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -47,10 +47,10 @@ return (
     <br />
     <div className="container">
       <h3 className="text-left shadow p-3 mb-5 bg-body rounded text-uppercase">Instructions</h3>
-      <h5>Insert a prompt to the AI and see what the computer has to tell to you</h5>
+      <h5 className="fw-light">Insert a prompt to the AI and see what the computer has to tell to you</h5>
       <form method="POST" onSubmit={onSubmit} className="mb-3">
         <label htmlFor="PromptAI" className="form-label badge rounded-pill text-bg-primary">Enter Prompt</label>
-        <textarea className="form-control" id="PromptAi" rows="3" onChange={(e) => setUserPrompt(e.target.value)} placeholder="Name a famous actor from the early 1900's" value={userPrompt}></textarea>
+        <textarea className="form-control" id="PromptAi" rows="3" onChange={(e) => setUserPrompt(e.target.value)} placeholder="Name a famous actor from the early 19th century" value={userPrompt}></textarea>
         <br />
         <input className="btn btn-primary" type="submit" id="query"></input>
       </form>
@@ -59,6 +59,14 @@ return (
     <div className="container">
       <h4 className="text-uppercase shadow p-3 mb-5 bg-body rounded">Responses</h4>
       <div className="container" id="responses">
+        {/* {responses.map(response => { return (
+          <div class="card w-75">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+        )})} */}
       </div>
     </div>
   </>
