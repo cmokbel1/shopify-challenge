5be21ccb67fd23a8b9235861c7a68ad6621bed32
Claude Mokbel <claudemokbel@gmail.com>
2022-05-20 07:15:17 -0700

    added a longer description 4 instructions

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   const [responses, setResponses] = useState([]);
 
 
-useEffect(() => {
+  useEffect(() => {
 
-}, [])
+  }, [])
   async function onSubmit(event) {
     event.preventDefault();
 
@@ -36,47 +36,51 @@ useEffect(() => {
         const jsonResponse = await response.json();
 
         setResponses([
-          { 
-          prompt: userPrompt,
-          aiResponse:jsonResponse.choices[0].text
-        }, ...responses]);
+          {
+            prompt: userPrompt,
+            aiResponse: jsonResponse.choices[0].text
+          }, ...responses]);
       }
     } catch (error) {
       console.log(error)
     }
   }
 
-return (
-  <>
-    <h1 className="text-light text-center bg-info shadow-lg p-3 mb-5">AI Sandbox</h1>
-    <br />
-    <h3 className="text-left shadow p-3 mb-5 bg-body rounded text-uppercase">Instructions</h3>
-    <div className="container">
-      
-      <h5 className="fw-light">Insert a prompt to the AI and see what the computer has to tell to you</h5>
-      <form method="POST" className="mb-3">
-        <label htmlFor="PromptAI" className="form-label badge rounded-pill text-bg-primary">Enter Prompt</label>
-        <textarea className="form-control" id="PromptAi" rows="3" onChange={(e) => setUserPrompt(e.target.value)} placeholder="Name a famous actor from the early 19th century" value={userPrompt}></textarea>
-        <br />
-        <input className="btn btn-primary" type="submit"  onClick={onSubmit} id="query"></input>
-      </form>
-    </div>
-    <br />
-    <h4 className="text-uppercase shadow p-3 mb-5 bg-body rounded">Responses</h4>
-    <div className="container shadow-lg">
-      <div className="container p-3 align-center" id="responses">
-        {responses.map((response, index) => { return (
-          <div key={index} className="card w-75 mb-3">
-          <div className="card-body">
-            <h5 className="card-title">prompt: {response.userPrompt}</h5>
-            <p className="card-text">response: {response.aiResponse}</p>
-          </div>
-        </div>
-        )})}
+  return (
+    <>
+      <nav>
+        <h1 className="text-light text-center bg-info shadow-lg p-3 mb-5">AI Sandbox</h1>
+      </nav>
+      <br />
+      <h3 className="text-left shadow p-3 mb-5 bg-body rounded text-uppercase">Instructions</h3>
+      <div className="container">
+
+        <h5 className="fw-light">Insert a prompt to the AI and see what the computer has to tell to you. There is a placeholder value to give you a suggestion, but you are not limited to this type of prompt. You can also ask the computer to tell you a story, or ask about the weather. The sky is the limit with this AI sandbox. </h5>
+        <form method="POST" className="mb-3">
+          <label htmlFor="PromptAI" className="form-label badge rounded-pill text-bg-primary">Enter Prompt</label>
+          <textarea className="form-control" id="PromptAi" rows="3" onChange={(e) => setUserPrompt(e.target.value)} placeholder="Name a famous actor from the early 19th century" value={userPrompt}></textarea>
+          <br />
+          <input className="btn btn-primary" type="submit" onClick={onSubmit} id="query"></input>
+        </form>
       </div>
-    </div>
-  </>
-);
+      <br />
+      <h4 className="text-uppercase shadow p-3 mb-5 bg-body rounded">Responses</h4>
+      <div className="container shadow-lg">
+        <div className="container p-3 align-center" id="responses">
+          {responses.map((response, index) => {
+            return (
+              <div key={index} className="card w-75 mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">prompt: {response.userPrompt}</h5>
+                  <p className="card-text">response: {response.aiResponse}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
